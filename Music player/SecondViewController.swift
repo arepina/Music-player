3e8cb27229e1741caa991f1e96ed71cac467fe5d
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-02-01 20:32:13 +0300

    Initial Commit

---
+++ Music player/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Music player
+//
+//  Created by Анастасия on 01.02.17.
+//  Copyright © 2017 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
